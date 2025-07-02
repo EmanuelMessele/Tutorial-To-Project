128c3ea2bef221d80d7d9e147e42f8c6dbdd9455
EmanuelMessele <emanuelmessele1@gmail.com>
2025-07-02 17:50:06 -0400

    got backened and front connected still having some trouble

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
  // we will be getting our transcripts from the youtube api and sending it over to OpenAi here
 
- const express = requires('express') // express for the first time to make http requests more convenient
+ const express = require('express') // express for the first time to make http requests more convenient
+ const cors = require('cors') // this will let our backend and front end talk : bypass CORS policy
  const app = express() // creating the server here is so much simpler than without Express
  const PORT = 5000
 
  
+app.use(cors()) // pass CORS policy
 app.use(express.json()); // registering middleware
 // before any of the routes are run, we will parse whatever incoming JSON
 
 app.post("/api/transcript", (req,res) => { // we dont have to check for the type of request if we use express
     // learn how to handle the request using express
+    console.log(req.body)
 });
 
-
+app.listen(PORT, () => {
+    console.log(`Example app listening at http:localhost:${PORT}`)
+}); 
--- src/components/LinkInsertCard.tsx
+++ src/components/LinkInsertCard.tsx
@@ -5,12 +5,25 @@ const LinkInsertCard = () => {
   // useState for chaning the state of our link
   const [link, setLink] = useState("");
 
-  const handleClick = () => {
+  const handleClick = async () => {
     // now we know it has access to the link
     console.log("Sending link to our backend...", link); // from here we will send it to our backend which we will then send to OpenAi API for suggestions
 
     // make our fetch request to backend here
     // learn more about how the front end makes that connection with the front before moving on
+
+    try {
+      const response = await fetch("http://localhost:5000/api/transcript", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ link }),
+      });
+
+      const data = await response.json();
+      console.log("Respone from backend:", data);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
