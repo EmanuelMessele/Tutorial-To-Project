41a6581d20a2fcee43264e06465b9b5bdc3475a2
EmanuelMessele <emanuelmessele1@gmail.com>
2025-07-07 18:12:29 -0400

    got the link part to actually go thru just needed a comma rather than a + in the console log, currently working with getting vid id to send over to youtbe api

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@
  const express = require('express') // express for the first time to make http requests more convenient
  const cors = require('cors') // this will let our backend and front end talk : bypass CORS policy
  const app = express() // creating the server here is so much simpler than without Express
+ const { getTranscript } = require("youtube-transcript") // here we will just get the one method we need from this object
  const PORT = 5000
 
  
@@ -12,8 +13,18 @@ app.use(express.json()); // registering middleware
 
 app.post("/api/transcript", (req,res) => { // we dont have to check for the type of request if we use express
     // learn how to handle the request using express
-    console.log(req.body)
+    console.log("We made it into the POST")
+    console.log("req.body", req.body) // comma vs the plus sign 
+
+    // youtube api send 
+    const { link } = req.body // another example of destructuring, instead it would be link = req.body.link
+    const videoID = getVidID(link)
 });
+
+function getVidID(link){
+    // goal of this function is to get our id to then send over, id helps find the right video
+    
+}
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http:localhost:${PORT}`)
